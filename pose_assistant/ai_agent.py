855f7d63d243fece88f200b5c5be7b7ef541462e
Younis <y.mageit@gmail.com>
2024-12-19 01:03:09 +0000

    Update script to use environment variables

--- pose_assistant/ai_agent.py
+++ pose_assistant/ai_agent.py
@@ -1,31 +1,20 @@
+import os
 from phi.agent import Agent
-#from phi.model.groq import Groq
 from phi.model.openai import OpenAIChat
-from dotenv import load_dotenv
 from phi.tools.duckduckgo import DuckDuckGo
 
-load_dotenv()
+# Get the OpenAI API key from the environment
+openai_api_key = os.getenv("OPENAI_API_KEY")
 
+if not openai_api_key:
+    raise ValueError("The OPENAI_API_KEY environment variable is not set.")
 
-# web_agent_groq = Agent(
-#     name="Web Agent",
-#     model= Groq(id="llama-3.3-70b-versatile"),
-#     tools=[DuckDuckGo()],
-#     instructions=["Only return the response to the user query and use available tools if necessary"],
-#     show_tool_calls=True,
-#     markdown=True,
-# )
-
-
+# Define the agent using the OpenAI model
 web_agent_openai = Agent(
     name="Web Agent",
-    model=OpenAIChat(id="gpt-4o"),
+    model=OpenAIChat(id="gpt-4o", api_key=openai_api_key),  # Pass the API key here
     tools=[DuckDuckGo()],
-    instructions=["Always include sources"],
+    instructions=["Only return the response to the user query and use available tools if necessary"],
     show_tool_calls=True,
     markdown=True,
 )
-
-
-
-#web_agent_groq.print_response("Who won the 2024 constructors championship? and how many points did they win it by?")
